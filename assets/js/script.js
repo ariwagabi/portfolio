cf73f3c41f6129b28a955b143f2751983b459fc3
ariwagabi <134345453+ariwagabi@users.noreply.github.com>
2024-01-13 21:00:27 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,36 @@
 $(document).ready(function () {
-  $(".menu-toggler").click(function () {
+  // Theme Toggler
+  $("[data-type-theme]").click(function () {
+    $("body").toggleClass("white-theme");
+  });
+
+  // Menu Toggler
+  $("[data-type-menu]").click(function () {
     $("#sm-nav").slideToggle(500);
     $("[data-type-menu='open']").toggle();
     $("[data-type-menu='close']").toggle();
   });
 
-  $("#hero-name-link").hover(function () {
-    $("#hero-contact").css("background-color", "var(--dimmed-yellow)");
-    $("#email-shown").css("display", "none");
-    $("#email-hidden").css("display", "block");
+  $("#hero-contact").hover(function () {
+    $("#hero-contact").css("background-color", "var(--yellow)");
+    $("#email-shown").toggle();
+    $("#email-hidden").toggle();
   },
     function () {
-      $("#hero-contact").css("border-color", "var(--dimmed-yellow)");
-      $("#hero-contact").css("background-color", "var(--dimmed-ash)");
-      $("#email-shown").css("display", "block");
-      $("#email-hidden").css("display", "none");
+      $("#hero-contact").css("background-color", "transparent");
+      $("#email-shown").toggle();
+      $("#email-hidden").toggle();
     }
   );
 
-  $(".hover-dim-yellow").hover(function () {
-    $(this).css("color", "var(--dimmed-yellow)");
+  $(".hero-social-icons").hover(function () {
+    $(this).css("color", "var(--yellow)");
   },
     function () {
-      $(this).css("color", "var(--pale-gray)");
+      $(this).css("color", "var(--gray)");
     }
   );
 
   const year = new Date().getFullYear();
-  $("#year").text(year + ". ")
+  $("#year").text(year + ". ");
 });
